2b933ccd5579b357d70d843a95701d6dd5d239ca
Christian Fischer <sw-dev@computerlyrik.de>
2013-08-29 20:30:19 +0200

    move to release 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de, greg@gregf.org"
 license          "Apache 2.0"
 description      "Installs/Configures prosody"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 recipe           'prosody', 'Installs sudo and configures prosody'
 
