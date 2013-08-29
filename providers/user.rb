697822df753e7a699df6710f9c2d8bc3fd9e08d0
Christian Fischer <sw-dev@computerlyrik.de>
2013-08-29 21:56:10 +0200

    deactivated inline_resource to work with chef 0.10

--- providers/user.rb
+++ providers/user.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-use_inline_resources
+#use_inline_resources
 
 def whyrun_supported?
   true
