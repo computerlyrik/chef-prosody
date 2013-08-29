0683e7bcca111a57fe2a4650913f33ba8f0183aa
Christian Fischer <sw-dev@computerlyrik.de>
2013-08-30 01:40:51 +0200

    removed required on attr (0.10 compatibility)

--- resources/module.rb
+++ resources/module.rb
@@ -1,7 +1,7 @@
 actions :add, :install, :delete, :enable, :disable
 default_action :add
 
-attribute :module, :name_attribute => true, :kind_of => String, :required => true, :name_attribute => true
+attribute :module, :name_attribute => true, :kind_of => String
 attribute :files, :kind_of => Hash, :default => Hash.new
 
 attr_accessor :configured
