02eb5d503d6120bade10e583922a18c5b33c9676
Christian Fischer <sw-dev@computerlyrik.de>
2013-08-30 01:38:50 +0200

    made module the name attribute of module resource

--- resources/module.rb
+++ resources/module.rb
@@ -1,7 +1,7 @@
 actions :add, :install, :delete, :enable, :disable
 default_action :add
 
-attribute :module, :name_attribute => true, :kind_of => String, :required => true
+attribute :module, :name_attribute => true, :kind_of => String, :required => true, :name_attribute => true
 attribute :files, :kind_of => Hash, :default => Hash.new
 
 attr_accessor :configured
